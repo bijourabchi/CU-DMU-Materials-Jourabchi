79ff4bcecc1f9f005172fd2bd5e4f71299aed63b
bijourabchi <bijo6049@colorado.edu>
2026-01-21 18:22:26 -0700

    HW1 work

--- hw/1-Probabilistic-Models/starter_code.jl
+++ hw/1-Probabilistic-Models/starter_code.jl
@@ -4,17 +4,34 @@ import DMUStudent.HW1
 # Problem 4
 #-------------
 
-# Here is a functional but incorrect answer for the programming question
 function f(a, bs)
-    return a*bs[1]
+
+    res = Vector{Vector{eltype(a)}}()
+    max = Vector{eltype(a)}()
+    
+    for b in bs
+        push!(res,a * b)
+    end
+
+    if length(res) == 1
+        return res[1]
+    end
+
+    res_matrix =  hcat(res...)
+
+    for row in eachrow(res_matrix)
+        push!(max,maximum(row))
+    end
+
+    return max
 end
 
 # You can can test it yourself with inputs like this
 a = [1.0 2.0; 3.0 4.0]
 @show a
-bs = [[1.0, 2.0], [3.0, 4.0]]
+bs = [[1.0, 2.0]]
 @show bs
 @show f(a, bs)
 
 # This is how you create the json file to submit
-HW1.evaluate(f, "your.gradescope.email@colorado.edu")
+HW1.evaluate(f, "bijan.jourabchi@colorado.edu")
