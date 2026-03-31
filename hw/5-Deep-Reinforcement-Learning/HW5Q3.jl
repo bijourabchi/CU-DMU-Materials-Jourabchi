e2dc46703395996c7091370f702b648f7e284d73
bijourabchi <bijo6049@colorado.edu>
2026-03-30 19:06:13 -0600

    HW5Q3 done kinda

--- hw/5-Deep-Reinforcement-Learning/HW5Q3.jl
+++ hw/5-Deep-Reinforcement-Learning/HW5Q3.jl
@@ -4,7 +4,11 @@ using POMDPTools: Deterministic, Uniform, SparseCat, FunctionPolicy, RolloutSimu
 using Statistics: mean
 using CommonRLInterface
 using Flux
+using Random
+using ProgressMeter
 using CommonRLInterface.Wrappers: QuickWrapper
+using ElectronDisplay
+ElectronDisplay.CONFIG.single_window = true
 import POMDPs
 
 
@@ -12,18 +16,78 @@ env = QuickWrapper(HW5.mc,
                    actions=[-1.0, -0.5, 0.0, 0.5, 1.0],
                    observe=mc->observe(mc)[1:2])
 
-function loss(Q, Q_target, s, a_ind, r, sp, done, γ)
+function loss(Q, Q_target, s, a_ind, r, sp, done)
     if ! done
-        return (r + γ*max(Q(sp)) - Q_target(s)[a_ind])^2 ## CHECK IF THIS IS RIGHT
-    else return 0 end
+        return (r + 0.99*maximum(Q_target(sp)) - Q(s)[a_ind])^2
+    else return (r-Q(s)[a_ind])^2 end
 end
 
 function dqn(env)
+    Q = Chain(Dense(2,128), relu,
+            Dense(128, length(actions(env)))) # Tune as needed
+    opt = Flux.setup(ADAM(0.0005), Q)
+    Q_target = deepcopy(Q)
+
+    N_EPSISODES = 3000
+    MAX_STEPS = 10000
+    WARMUP_STEPS = 2000 # Idea is to let it explore for a while to gain experience_tuples before training to ensure NN has data
+    REPLAY_CAPACITY = 200_000
+    TRAIN_SAMPLES_PER_EPISODE = 1000
+
+    buffer = []
+    grad_steps = 0
+
+    function policy(s,eps)
+        if rand() < eps
+            return rand(1:length(actions(env)))
+        else
+            
+            return argmax(i -> Q(s)[i], 1:length(actions(env)))
+        end
+    end
+
+    @showprogress for i = 1:N_EPSISODES
+        reset!(env)
+        count = 0
+        eps = max(0.01, 1-i/N_EPSISODES)
+        done = false
+
+        while !done && count < MAX_STEPS
+            # Create our experience tuple
+            s = observe(env)
+            a_ind = policy(s,eps)
+            r = act!(env, actions(env)[a_ind])
+            sp = observe(env)
+            done = terminated(env)
+
+            experience_tuple = (s, a_ind, r, sp, done)
+            push!(buffer, experience_tuple)
+            if length(buffer) > REPLAY_CAPACITY
+                popfirst!(buffer) # drop oldest transition
+            end
+             
+            count += 1
+        end
+        
+        Q_target = deepcopy(Q)
+
+        if length(buffer) >= WARMUP_STEPS
+            n_updates = min(TRAIN_SAMPLES_PER_EPISODE, length(buffer))
+            for data in rand(buffer, n_updates)
+                # this runs a forward and backward pass to calculate the loss and gradient
+                loss_value, grads = Flux.withgradient(loss, Q, Q_target, data...)
+
+                # this will take a gradient step
+                Flux.update!(opt, Q, grads[1])
+            end
+        end
+    end
+    return Q
 end
 
 Q = dqn(env)
 
-HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], n_episodes=100) # you will need to remove the n_episodes=100 keyword argument and add your email as a positional argument to create a json file; evaluate needs to run 10_000 episodes to produce a json
+@show HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], "bijan.jourabchi@colorado.edu") # you will need to remove the n_episodes=100 keyword argument and add your email as a positional argument to create a json file; evaluate needs to run 10_000 episodes to produce a json
 
 #----------
 # Rendering
@@ -37,4 +101,3 @@ using Plots
 xs = -3.0f0:0.1f0:3.0f0
 vs = -0.3f0:0.01f0:0.3f0
 heatmap(xs, vs, (x, v) -> maximum(Q([x, v])), xlabel="Position (x)", ylabel="Velocity (v)", title="Max Q Value")
-
